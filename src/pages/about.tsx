62c1e760d57fd7fa2e9274ebcb4b0ffcff12cc29
Tony Polinelli <tony.polinelli@auspost.com.au>
2017-11-27 23:56:46 +1100

    async page example, dependancy fixes

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,21 @@
 import * as React from 'react';
 import App from '../components/App';
 
-export default () =>
-  <App>
-    <p>About Pages</p>
-  </App>;
+interface Props {
+  message: string
+}
+
+export default class About extends React.Component<Props> {
+  
+    static async getInitialProps({ query }) {
+      const content = await new Promise(res => setTimeout(res, 1000))
+      return {
+        message: '!!'
+      }
+    }
+    
+    render() {
+      return (<App><p>About Page ${this.props.message}</p></App>)
+    }
+  
+  }
