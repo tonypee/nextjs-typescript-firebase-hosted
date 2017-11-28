2faca9d0a3e1bdde4b7438090f31c27dc4311c51
Tony Polinelli <tony.polinelli@auspost.com.au>
2017-11-28 11:58:05 +1100

    add static assets

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,9 @@ import Link from 'next/link';
 
 export default ({ pathname }: { pathname?: string}) =>
   <header>
+    <p>
+      <img src="/static/logo.png" />
+    </p>
     <Link href="/">
       <a className={pathname === '/' && 'is-active' || ''}>Home</a>
     </Link>{' '}
